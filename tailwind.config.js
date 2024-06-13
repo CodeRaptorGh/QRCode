054ea8007af417ea8ce4b6eb312308410b1b9431
= <=>
2024-06-13 11:17:48 +0000

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        lightGrey: "#D5E1EF",
+        naturalBlue: "#2C7DFA",
+        blueShade: "#3685FF",
+        darkNavy: "#1F314F",
+        grey: "#7D889E",
+      },
+    },
+  },
+  plugins: [],
+};
